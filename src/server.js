8092e0923c4187390aecefd27c5e2162785127ba
denis.shtupa <denis.shtupa@3i-solutions.net>
2020-12-13 12:46:37 +0100

    package.json changes

--- src/server.js
+++ src/server.js
@@ -5,9 +5,10 @@ const app = express();
 
 app.use(express.static('./dist/movie-db-app'));
 
-app.get('/*', function(req, res) {
-  res.sendFile('index.html', {root: 'dist/movie-db-app/'}
-);
+app.get('/*', function (req, res) {
+  res.sendFile('index.html', {
+    root: 'dist/movie-db-app/'
+  });
 });
 
 app.listen(process.env.PORT || 4200);
